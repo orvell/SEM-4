6d72cd06f0cf8b4ca394dea180fd76e509857b57
ORVELL FERREIRA <56030574+orvell@users.noreply.github.com>
2020-02-06 23:01:15 +0530

    Update pract1_primefactors.py

--- python programs/pract1_primefactors.py
+++ python programs/pract1_primefactors.py
@@ -1,22 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  6 22:53:05 2020
-
-@author: DRUELL
-"""
-
 def check_prime( val ):
-    """
-    Check which factors are prime
-    """
+   
     for i in range(2, val):
         if(val%i == 0):
             return 0
     return 1
 
 val = int(input("Enter number to find prime factors : "))
-#print(val)
-
 
 factors = []
 primefactors = []
